fd53d732028148f5f26a878a27cdfca107c7bb36
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 02:14:29 +0300

    Added views for creating cloth object

--- Cloths/ClothsApp/views.py
+++ Cloths/ClothsApp/views.py
@@ -20,6 +20,14 @@ class AllClothsView(APIView):
         # serializer = ClothSerializer(instance=cloths_amount, many=True)
         # return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request : Request):
+        print(request.data)
+        serializer = ClothSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class ConcreteClothView(APIView):
     def get(self, request: Request, cloth_uuid):
          try:
