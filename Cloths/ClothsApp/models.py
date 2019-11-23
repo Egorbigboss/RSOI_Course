366c6c277eccce42fc32ef4fb77593ab31d34a49
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 02:13:50 +0300

    Changed cloth model according to new views

--- Cloths/ClothsApp/models.py
+++ Cloths/ClothsApp/models.py
@@ -7,7 +7,7 @@ class Cloth(models.Model):
     # belongs_to_order_uuid = models.IntegerField(null=False)
     type_of_cloth = models.CharField(null=True, max_length=2048)
     # cloth_uuid = models.UUIDField(null=True)
-    days_for_clearing = models.IntegerField(null = False)
+    days_for_clearing = models.IntegerField(default = 0)
 
     def __str__(self):
         return f'Cloth, uuid={self.uuid}'
