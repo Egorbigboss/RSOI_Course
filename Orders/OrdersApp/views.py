964c8329f1a6c5d513764678f520ef422c1d88d1
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-23 17:43:43 +0300

    Fixed views and urls after revert

--- Orders/OrdersApp/views.py
+++ Orders/OrdersApp/views.py
@@ -7,22 +7,21 @@ from django.db.models import Q
 
 # Create your views here.
 
-class AllOrdersView(ListAPIView):
-
-         def get(self, request: Request):
-            if len(request.query_params) == 0:
+class AllOrdersView(APIView):
+    def get(self, request: Request):
+        if len(request.query_params) == 0:
             # Вообще все
-                return Response(OrderSerializer(instance=Order.objects.all(), many=True).data)
+            return Response(OrderSerializer(instance=Order.objects.all(), many=True).data)
         # Для определенного юзера
-         try:
+        try:
             user_id = request.query_params['user_id']
-         except KeyError:
+        except KeyError:
             return Response({'error': 'Wrong query params'}, status=status.HTTP_400_BAD_REQUEST)
-         orders_amount = Order.objects.filter(Q(belongs_to_user_id = user_id))
-         if len(orders_amount) == 0:
-             return Response(status=status.HTTP_404_NOT_FOUND)
-         serializer = OrderSerializer(instance=orders_amount, many=True)
-         return Response(data=serializer.data, status=status.HTTP_200_OK)
+        orders_amount = Order.objects.filter(Q(belongs_to_user_id = user_id))
+        if len(orders_amount) == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderSerializer(instance=orders_amount, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 class ConcreteOrderView(APIView):
 
