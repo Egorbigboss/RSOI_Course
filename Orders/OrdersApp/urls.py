cd6f85562e0e6455edfa30ed02a52491f56a4d27
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-23 18:49:10 +0300

    Fixed ordersapp urls

--- Orders/OrdersApp/urls.py
+++ Orders/OrdersApp/urls.py
@@ -3,6 +3,6 @@ from OrdersApp import views
 from django.urls import path
 
 urlpatterns = [
-    url(r'^orders/all/', views.AllOrdersView.as_view()),
-    url(r'^orders/(?P<order_uuid>\w+)/$', views.ConcreteOrderView.as_view()),
+    url(r'^orders/all/$', views.AllOrdersView.as_view()),
+    url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$', views.ConcreteOrderView.as_view()),
 ]
