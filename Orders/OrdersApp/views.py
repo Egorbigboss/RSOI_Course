20c1db15dad6f2644e9a361f0c0c13e66d0e915d
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 02:18:47 +0300

    Added views for creating orders

--- Orders/OrdersApp/views.py
+++ Orders/OrdersApp/views.py
@@ -8,6 +8,7 @@ from django.db.models import Q
 # Create your views here.
 
 class AllOrdersView(APIView):
+
     def get(self, request: Request):
         if len(request.query_params) == 0:
             # Вообще все
@@ -22,6 +23,13 @@ class AllOrdersView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = OrderSerializer(instance=orders_amount, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request : Request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ConcreteOrderView(APIView):
 
