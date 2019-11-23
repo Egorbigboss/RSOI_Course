66f064a26f784e9f2d971c8e804e01a6b26afa24
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-23 17:14:13 +0300

    Revert "Orders service"
    This reverts commit 969e69009fa5d411bed76c245eea22df94293449.

--- Orders/OrdersApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from OrdersApp.models import Order
-
-
-admin.site.register(Order)
--- Orders/OrdersApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from OrdersApp import views
-from django.urls import path
-
-urlpatterns = [
-    url(r'^messages/all/', views.AllOrdersView.as_view()),
-    url(r'^orders/(?P<order_uuid>\w+)/$', views.ConcreteOrderView.as_view()),
-]
