f0cd82647e91a515d4df28d19675fbc987f08494
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-23 18:20:51 +0300

    Added basic Clothspp view

--- Cloths/ClothsApp/views.py
+++ Cloths/ClothsApp/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView, Response, Request
+from ClothsApp.serializers import ClothSerializer
+from ClothsApp.models import Cloth
+from django.db.models import Q
 
 # Create your views here.
+
+class AllClothsView(APIView):
+    def get(self, request: Request):
+            return Response(ClothSerializer(instance=Cloth.objects.all(), many=True).data)
+        # # Для определенного юзера
+        # try:
+        #     user_id = request.query_params['user_id']
+        # except KeyError:
+        #     return Response({'error': 'Wrong query params'}, status=status.HTTP_400_BAD_REQUEST)
+        # cloths_amount = Cloth.objects.filter(Q(belongs_to_user_id = user_id))
+        # if len(cloths_amount) == 0:
+        #     return Response(status=status.HTTP_404_NOT_FOUND)
+        # serializer = ClothSerializer(instance=cloths_amount, many=True)
+        # return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+class ConcreteClothView(APIView):
+    def get(self, request: Request, cloth_uuid):
+         try:
+             cloth = Cloth.objects.get(pk=cloth_uuid)
+         except Cloth.DoesNotExist:
+             return Response(status=status.HTTP_404_NOT_FOUND)
+         serializer = clothSerializer(instance=cloths_amount)
+         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request: Request, cloth_uuid):
+         try:
+             cloth = Cloth.objects.get(pk=cloth_uuid)
+         except Cloth.DoesNotExist:
+             return Response(status=status.HTTP_404_NOT_FOUND)
+         cloth.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AddClothToClothView(APIView)
