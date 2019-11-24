3428798aefcd39b42836e65a4d17ded2e4769cb2
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 18:09:28 +0300

    Added urls for new views

--- Orders/OrdersApp/urls.py
+++ Orders/OrdersApp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     url(r'^orders/all/$', views.AllOrdersView.as_view()),
-    url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$', views.ConcreteOrderView.as_view()),
+    url(r'^orders/user/(?P<user_id>[0-9a-f-]+)/$',views.ConcreteUserOrdersView.as_view()),
+    url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$', views.ConcreteOrderView.as_view())
 ]
