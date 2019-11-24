499ed08afc22ad54c575ecbbbc60fc19daadb7d9
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 18:05:56 +0300

    Fixed comments

--- Cloths/ClothsApp/models.py
+++ Cloths/ClothsApp/models.py
@@ -4,9 +4,7 @@ from django.db import models
 
 class Cloth(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # belongs_to_order_uuid = models.IntegerField(null=False)
     type_of_cloth = models.CharField(null=True, max_length=2048)
-    # cloth_uuid = models.UUIDField(null=True)
     days_for_clearing = models.IntegerField(default = 0)
 
     def __str__(self):
