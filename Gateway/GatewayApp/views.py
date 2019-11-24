5edea221c35ec38e133579e2418652d2eb8aa896
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 18:08:21 +0300

    Added views for creating orders for concrete user

--- Gateway/GatewayApp/views.py
+++ Gateway/GatewayApp/views.py
@@ -11,22 +11,28 @@ class CreateCloth(APIView):
         response_json, code = Requester.create_cloth(type_of_cloth=request.data['type_of_cloth'], days_for_clearing=request.data['days_for_clearing'])
         return Response(data=response_json, status=code)
 
-class CreateOrder(APIView):
-    def post(self, request : Request):
-        order_data = {'text','cloth_type'}.intersection(request.data.keys())
-        if (len(order_data)) != 2:
+class CreateOrderView(APIView):
+    def post(self, request : Request,user_id):
+        print("User ID - ",user_id)
+        order_data = {'text','type_of_cloth','days_for_clearing'}.intersection(request.data.keys())
+        if (len(order_data)) != 3:
             return Response ({'error': 'Body must have text and days_for_clearing'},status=400)
-        response_json, code = Requester.create_order(text=request.data['text'], type_of_cloth=request.data['days_for_clearing'])
+        response_json, code = Requester.create_order(belongs_to_user_id=user_id, text=request.data['text'], type_of_cloth=request.data['type_of_cloth'], days_for_clearing=request.data['days_for_clearing'])
         return Response(data=response_json, status=code)
 
-class ClothsView(APIView):
-    def get(self, request: Request):
-        data, code = Requester.get_cloths()
-        return Response(data, status=code)
+# class ClothsView(APIView):
+#     def get(self, request: Request):
+#         data, code = Requester.get_cloths()
+#         return Response(data, status=code)
 
 class ConcreteClothView(APIView):
     def get(self, request: Request, cloth_uuid):
         data, code = Requester.get_concrete_cloth(str(Cloth_uuid))
+        return Response(data, status=code)
+
+class ConcreteUserOrdersView(APIView):
+    def get(self, request: Request, user_id):
+        data, code = Requester.get_orders(user_id=user_id)
         return Response(data, status=code)
 
 
