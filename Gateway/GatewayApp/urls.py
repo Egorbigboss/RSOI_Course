dc579b93f61671d55f73470b3d1074ec1df1f3ff
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-24 18:07:38 +0300

    Added new urls for creating orders for specific user

--- Gateway/GatewayApp/urls.py
+++ Gateway/GatewayApp/urls.py
@@ -3,13 +3,15 @@ from GatewayApp import views
 
 
 urlpatterns = [
-     url(r'^cloths/$', views.ClothsView.as_view()),
+     # url(r'^cloths/$', views.ClothsView.as_view()),
+     # url(r'^cloths/create/$',views.CreateCloth.as_view()),
      url(r'^cloths/(?P<cloth_uuid>[0-9a-f-]+)/$',
          views.ConcreteClothView.as_view()),
-     url(r'^cloths/create/$',views.CreateCloth.as_view()),
 
      url(r'^orders/$', views.OrdersView.as_view()),
      url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$',
          views.ConcreteOrderView.as_view()),
-     url(r'orders/create/$',views.CreateOrder.as_view())
+     url(r'^orders/user/(?P<user_id>\d+)/create/$',views.CreateOrderView.as_view()),
+
+     url(r'^orders/user/(?P<user_id>\d+)/$',views.ConcreteUserOrdersView.as_view())
 ]
