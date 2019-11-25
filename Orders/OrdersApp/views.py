18748f569876785cc7d37a9c8cbc640447719bf0
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-26 01:05:30 +0300

    Changed view class for pagination

--- Orders/OrdersApp/views.py
+++ Orders/OrdersApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.views import APIView, Response, Request
 from OrdersApp.serializers import OrderSerializer
 from OrdersApp.models import Order
@@ -9,28 +10,16 @@ from django.db.models import Q
 
 
 
-class ConcreteUserOrdersView(APIView):
-    def get(self,request : Request, user_id):
-        request=self.request
-        if user_id == None:
-            return Response(OrderSerializer(instance=Order.objects.all(), many=True).data)
-        orders_amount = Order.objects.filter(Q(belongs_to_user_id = user_id))
-        if len(orders_amount) == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = OrderSerializer(instance=orders_amount, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+class ConcreteUserOrdersView(ListCreateAPIView):
+    serializer_class = OrderSerializer
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Order.objects.filter(Q(belongs_to_user_id = user_id))
 
-    def post(self, request : Request,user_id):
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class AllOrdersView(APIView):
-    def get(self,request : Request):
-        request=self.request
-        return Response(OrderSerializer(instance=Order.objects.all(), many=True).data)
+class AllOrdersView(ListCreateAPIView):
+    serializer_class = OrderSerializer
+    def get_queryset(self):
+        return Order.objects.all()
 
 class ConcreteOrderView(APIView):
     def get(self, request: Request, order_uuid):
