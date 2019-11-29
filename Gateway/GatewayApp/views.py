fa6590d6de072a8ef4009f74fd22816ed287734e
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-29 14:25:49 +0300

    Added patch method for ConcreteClothView

--- Gateway/GatewayApp/views.py
+++ Gateway/GatewayApp/views.py
@@ -3,13 +3,14 @@ from GatewayApp.inter_service_requests import Requester
 
 
 
-class CreateCloth(APIView):
+class CreateClothView(APIView):
     def post(self, request : Request):
         cloth_data = {'type_of_cloth', 'days_for_clearing'}.intersection(request.data.keys())
         if (len(cloth_data)) != 2:
             return Response ({'error': 'Body must have type_of_cloth and days_for_clearing'},status=400)
         response_json, code = Requester.create_cloth(type_of_cloth=request.data['type_of_cloth'], days_for_clearing=request.data['days_for_clearing'])
         return Response(data=response_json, status=code)
+
 
 class CreateOrderView(APIView):
     def post(self, request : Request):
@@ -28,6 +29,10 @@ class ConcreteClothView(APIView):
     def get(self, request: Request, cloth_uuid):
         data, code = Requester.get_concrete_cloth(str(Cloth_uuid))
         return Response(data, status=code)
+
+    def patch(self, request : Request, cloth_uuid):
+        response_json, code = Requester.patch_concrete_cloth(cloth_uuid, data=request.data)
+        return Response(data=response_json, status=code)
 
 class ConcreteUserOrdersView(APIView):
     def get(self, request: Request, user_id):
