7a24d197bd66c6b48617bcfbb2d8a4296fd7d9e3
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-29 14:00:02 +0300

    Added urls for Delivery app

--- Gateway/GatewayApp/urls.py
+++ Gateway/GatewayApp/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
      url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$',
          views.ConcreteOrderView.as_view()),
      url(r'^orders/user/create/$',views.CreateOrderView.as_view()),
+     url(r'^orders/user/(?P<user_id>\d+)/$',views.ConcreteUserOrdersView.as_view()),
 
-     url(r'^orders/user/(?P<user_id>\d+)/$',views.ConcreteUserOrdersView.as_view())
+     url(r'^delivery/user/(?P<user_id>\d+)/$', views.ConcreteUserDeliveryView.as_view()),
+     url(r'^delivery/user/create/$',views.CreateDeliveryList.as_view())
 ]
