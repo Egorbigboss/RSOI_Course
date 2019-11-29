c9b2ca477d6b018f3c929aba55643ae16be0e96d
Egor Vedyashkin <egorbigboss@gmail.com>
2019-11-29 16:49:30 +0300

    Added patch method for ConcreteOrderView

--- Orders/OrdersApp/views.py
+++ Orders/OrdersApp/views.py
@@ -22,12 +22,18 @@ class AllOrdersView(ListCreateAPIView):
         return Order.objects.all()
 
 class ConcreteOrderView(APIView):
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+             return Response(status=status.HTTP_404_NOT_FOUND)
+
     def get(self, request: Request, order_uuid):
          try:
              order = Order.objects.get(pk=order_uuid)
          except Order.DoesNotExist:
              return Response(status=status.HTTP_404_NOT_FOUND)
-         serializer = orderSerializer(instance=order)
+         serializer = OrderSerializer(instance=order)
          return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request: Request, order_uuid):
@@ -37,3 +43,14 @@ class ConcreteOrderView(APIView):
              return Response(status=status.HTTP_404_NOT_FOUND)
          order.delete()
          return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request: Request, order_uuid):
+        try:
+            order = Order.objects.get(pk=order_uuid)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderSerializer(instance=order, data=request.data, partial=True) # set partial=True to update a data partially
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
