5a7a742d72449799fd79613c58b26c0ef3653e41
Egor Vedyashkin <egorbigboss@gmail.com>
2019-12-13 14:31:19 +0300

    Renamed cloths dashboard

--- Gateway/frontend/src/components/cloths/Dashboard.js
+++ Gateway/frontend/src/components/cloths/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import Form from './Form';
 import Cloths from './Cloths';
 
-export default function Dashboard() {
+export default function Clth_Dashboard() {
     return (
         <Fragment>
             <Form />
