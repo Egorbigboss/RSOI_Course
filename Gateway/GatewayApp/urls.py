6a4d47f3a96224c1075669a8a5bda46bfc98ea16
Egor Vedyashkin <egorbigboss@gmail.com>
2020-01-16 15:58:56 +0300

    Added urls for oauth and changed old users parsing

--- Gateway/GatewayApp/urls.py
+++ Gateway/GatewayApp/urls.py
@@ -12,8 +12,15 @@ urlpatterns = [
      url(r'^orders/(?P<order_uuid>[0-9a-f-]+)/$',
          views.ConcreteOrderView.as_view()),
      url(r'^orders/user/create/$',views.CreateOrderView.as_view()),
-     url(r'^orders/user/(?P<user_id>\d+)/$',views.ConcreteUserOrdersView.as_view()),
+    #  url(r'^orders/user/(?P<user_id>\d+)/$',views.ConcreteUserOrdersView.as_view()),
+    url(r'^orders/user/$',views.ConcreteUserOrdersView.as_view()),
 
      url(r'^delivery/user/(?P<user_id>\d+)/$', views.ConcreteUserDeliveryView.as_view()),
-     url(r'^delivery/user/create/$',views.CreateDeliveryList.as_view())
+     url(r'^delivery/user/create/$',views.CreateDeliveryList.as_view()),
+
+     url(r'^users/authenticate/$',views.AuthenicateUser.as_view()),
+     url(r'^users/user/$',views.GetUser.as_view()),
+
+     url(r'^oauth/login/$', views.OLoginView.as_view()),
+     url(r'^oauth/redirect/$', views.ORedirectView.as_view()),
 ]
